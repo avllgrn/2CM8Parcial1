9341504f4692f7aacb38448de113b94e649427f2
avllgrn <avillagran@ipn.mx>
2026-03-02 14:47:27 -0600

    Genera con ceros y muestra Matriz m filas, n columnas

--- ejemplo.py
+++ ejemplo.py
@@ -1,6 +1,28 @@
 from os import system
 
+def generaMatrizCeros(m, n):
+    M = []
+    for i in range(m):
+        fila = []
+        for j in range(n):
+            fila.append( 0 )
+        M.append( fila )
+    return M
+
+def muestraMatriz(M):
+    m = len(M)
+    n = len( M[0] )
+    for i in range(m):
+        for j in range(n):
+            print(M[i][j], end='\t')
+        print()
+
 if __name__== '__main__':
     system('cls')
 
-    print('Hola, mundo!')
+    m = int(input('Cuantas filas? '))
+    n = int(input('Cuantas columnas? '))
+
+    M = generaMatrizCeros(m, n)
+    print(M)
+    muestraMatriz(M)
