b980dd88075b3bf4537f6b82b8cb959ac0f33e86
avllgrn <avillagran@ipn.mx>
2026-03-02 14:49:22 -0600

    Lee matriz

--- ejemplo.py
+++ ejemplo.py
@@ -1,4 +1,5 @@
 from os import system
+from random import randrange
 
 def generaMatrizCeros(m, n):
     M = []
@@ -17,12 +18,21 @@ def muestraMatriz(M):
             print(M[i][j], end='\t')
         print()
 
+def leeMatriz(m, n):
+    M = []
+    for i in range(m):
+        fila = []
+        for j in range(n):
+            fila.append( int(input(f'Ingresa [{i}][{j}] ')) )
+        M.append( fila )
+    return M
+
 if __name__== '__main__':
     system('cls')
 
     m = int(input('Cuantas filas? '))
     n = int(input('Cuantas columnas? '))
 
-    M = generaMatrizCeros(m, n)
+    M = leeMatriz(m, n)
     print(M)
     muestraMatriz(M)
