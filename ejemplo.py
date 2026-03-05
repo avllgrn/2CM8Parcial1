eb9de4b191ea2b9e6e43ad4c2202cef51541ea11
avllgrn <avillagran@ipn.mx>
2026-03-05 07:19:03 -0600

    Mayor y menor en Vector

--- ejemplo.py
+++ ejemplo.py
@@ -1,167 +1,6 @@
 from os import system
 from random import randrange
 
-def generaMatrizCeros(m, n):
-    M = []
-    for i in range(m):
-        fila = []
-        for j in range(n):
-            fila.append( 0 )
-        M.append( fila )
-    return M
-
-def muestraMatriz(M):
-    m = len(M)
-    n = len( M[0] )
-    for i in range(m):
-        for j in range(n):
-            print(M[i][j], end='\t')
-        print()
-
-def leeMatriz(m, n):
-    M = []
-    for i in range(m):
-        fila = []
-        for j in range(n):
-            fila.append( int(input(f'Ingresa [{i}][{j}] ')) )
-        M.append( fila )
-    return M
-
-def generaMatrizAleatorios(m, n, inf, sup):
-    M = []
-    for i in range(m):
-        fila = []
-        for j in range(n):
-            fila.append( randrange(inf, sup) )
-        M.append( fila )
-    return M
-
-def generaMatrizIdentidad(n):
-    M = []
-    for i in range(n):
-        fila = []
-        for j in range(n):
-            if i==j:
-                fila.append( 1 )
-            else:
-                fila.append( 0 )
-        M.append( fila )
-    return M
-
-def generaMatrizConteo(m, n, ini, inc):
-    M = []
-    contador = ini
-    for i in range(m):
-        fila = []
-        for j in range(n):
-            fila.append( contador )
-            contador += inc
-        M.append( fila )
-        
-    return M
-
-def generaMatrizConteoIzqDerArrAba(m, n, ini, inc):
-    M = generaMatrizCeros(m,n)
-
-    contador = ini
-    for i in range(m):
-        for j in range(n):
-            M[i][j] = contador
-            contador += inc
-        
-    return M
-
-def generaMatrizConteoArrAbaIzqDer(m, n, ini, inc):
-    M = generaMatrizCeros(m,n)
-
-    contador = ini
-    for j in range(n):
-        for i in range(m):
-            M[i][j] = contador
-            contador += inc
-        
-    return M
-
-def generaMatrizConteoDerIzqArrAba(m, n, ini, inc):
-    M = generaMatrizCeros(m,n)
-
-    contador = ini
-    for i in range(m):
-        for j in range(n-1, -1, -1):
-            M[i][j] = contador
-            contador += inc
-        
-    return M
-
-def generaMatrizConteoArrAbaDerIzq(m, n, ini, inc):
-    M = generaMatrizCeros(m,n)
-
-    contador = ini
-    for j in range(n-1, -1, -1):
-        for i in range(m):
-            M[i][j] = contador
-            contador += inc
-
-    return M
-
-def posiciones(m,n):
-    for i in range(m):
-        for j in range(n):
-            print(f'[{i}][{j}]', end='\t')
-        print()
-
-def triangularInferior(n):
-    M = generaMatrizCeros(n,n)
-    for i in range(n):
-        for j in range(n):
-            if i>=j:
-                M[i][j] = randrange(100)
-    return M
-
-def triangularSuperior(n):
-    M = generaMatrizCeros(n,n)
-    for i in range(n):
-        for j in range(n):
-            if i<=j:
-                M[i][j] = randrange(100)
-    return M
-    
-def triangularInferior(n):
-    M = generaMatrizCeros(n,n)
-    for i in range(n):
-        for j in range(n):
-            if i>=j:
-                M[i][j] = randrange(100)
-    return M
-
-def triangularSuperior(n):
-    M = generaMatrizCeros(n,n)
-    for i in range(n):
-        for j in range(n):
-            if i<=j:
-                M[i][j] = randrange(100)
-    return M
-    
-def generaTriangularInferiorIzqDerArrAba(n, ini, inc):
-    M = generaMatrizCeros(n,n)
-    contador = ini
-    for i in range(n):
-        for j in range(n):
-            if i>=j:
-                M[i][j] = contador
-                contador += inc
-    return M
-
-def generaTriangularSuperiorIzqDerArrAba(n, ini, inc):
-    M = generaMatrizCeros(n,n)
-    contador = ini
-    for i in range(n):
-        for j in range(n):
-            if i<=j:
-                M[i][j] = contador
-                contador += inc
-    return M
-
 def generaVector(n):
     V = []
     for i in range(n):
@@ -180,33 +19,39 @@ def copiaVector(V):
         Copia.append( V[i] )
     return Copia
 
-def copiaMatriz(M):
-    m = len(M)
-    n = len(M[0])
-    Copia = generaMatrizCeros(m, n)
-    for i in range(m):
-        for j in range(n):
-            Copia[i][j] = M[i][j]
-    return Copia
+def menorEnVector(V):
+    n = len(V)
+    menor = V[0]
+    posicionMenor = 0
+    for i in range(n):
+        if V[i]<menor:
+            menor=V[i]
+            posicionMenor = i
+    return posicionMenor
 
-def generaTranspuesta(M):
-    mT = len(M[0])
-    nT = len(M)
-    MT = generaMatrizCeros(mT, nT)
-    for i in range(m):
-        for j in range(n):
-           MT[j][i] = M[i][j]
-    return MT
+def mayorEnVector(V):
+    n = len(V)
+    mayor = V[0]
+    posicionMayor = 0
+    for i in range(n):
+        if V[i]>mayor:
+            mayor=V[i]
+            posicionMayor = i
+    return posicionMayor
 
 if __name__== '__main__':
     system('cls')
-   
-    m = int(input('Cuántas filas? '))
-    n = int(input('Cuántas columnas? '))
-    M = generaMatrizAleatorios(m,n,0,100)
-    MT = generaTranspuesta(M)
 
-    print('\nM')
-    muestraMatriz(M)
-    print('\nMT')
-    muestraMatriz(MT)
+    n = int(input('Cuántos datos? '))
+    V = generaVector(n)
+    print('V')
+    muestraVector(V)
+    print()
+
+    pMenor = menorEnVector(V)
+    menor = V[pMenor]
+    pMayor = mayorEnVector(V)
+    mayor = V[pMayor]
+
+    print(f'El valor menor es {menor} y está en [{pMenor}]')
+    print(f'El valor mayor es {mayor} y está en [{pMayor}]')
