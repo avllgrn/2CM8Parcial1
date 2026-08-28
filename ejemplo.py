4a369ac0c8c67ecd06971589e59415b8c245cbe7
avllgrn <avillagran@ipn.mx>
2026-08-28 12:03:19 -0600

    Por que una lista es ordenada

--- ejemplo.py
+++ ejemplo.py
@@ -3,49 +3,18 @@ from subprocess import run
 if __name__=='__main__':
     run('cls', shell=True)
 
-    # Collecciones ordenadas
-    # Collecciones desordenadas
-    # Collecciones mutables
-    # Collecciones inmutables
-
-    lista = list()
+    lista = [51, -3, 5, 71, 29]
     print(lista, type(lista), len(lista))
+    print()
 
-    lista = []
-    print(lista, type(lista), len(lista))
+    n = len(lista)
+    for i in range(n):
+        print( f'lista[{i}] = {lista[i]}' )
+    print()
 
-    lista.append(51)
-    print(lista, type(lista), len(lista))
+    i=0 
+    while i<n:
+        print( f'lista[{i}] = {lista[i]}' )
+        i += 1
+    print()
 
-    lista.append(14)
-    print(lista, type(lista), len(lista))
-
-    lista.append(38)
-    print(lista, type(lista), len(lista))
-
-    lista.append(29)
-    print(lista, type(lista), len(lista))
-
-    lista.append(71)
-    print(lista, type(lista), len(lista))
-    print('\n\n')
-
-    print(f'Salió {lista.pop()}')
-    print(lista, type(lista), len(lista))
-
-    print(f'Salió {lista.pop()}')
-    print(lista, type(lista), len(lista))
-
-    print(f'Salió {lista.pop()}')
-    print(lista, type(lista), len(lista))
-
-    print(f'Salió {lista.pop()}')
-    print(lista, type(lista), len(lista))
-
-    print(f'Salió {lista.pop()}')
-    print(lista, type(lista), len(lista))
-
-    # Nunca pop a lista vacía
-    # print('\n\n')
-    # print(f'Salió {lista.pop()}')
-    # print(lista, type(lista), len(lista))
