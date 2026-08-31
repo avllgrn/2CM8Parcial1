27d1c28eaac73c10ab99f0857e76942545fc3c22
avllgrn <avillagran@ipn.mx>
2026-08-31 12:50:07 -0600

    Promedio de tres parciales

--- ejemplo.py
+++ ejemplo.py
@@ -4,35 +4,26 @@ from random import randrange
 def generaVectorAleatorio(n):
     V = []
     for i in range(n):
-        V.append( randrange(100) )
+        V.append( randrange(100)/10 )
     return V
-
-def sumaDosVectores(A, B):
-    n = len(A)
-    C = []
-    for i in range(n):
-        C.append( A[i] + B[i] )    
-    return C
-
-def muestraVectores(X, Y, Z):
-    n = len(X)
-    for i in range(n):
-        print(f'[{i}]\t{X[i]} + {Y[i]}\t= {Z[i]}')
 
 if __name__=='__main__':
     run('cls', shell=True)
 
-    n1 = int(input('Dame n1 '))
-    n2 = int(input('Dame n2 '))
+    n = int(input('¿Cuántos alumnos? '))
 
-    if n1==n2:
-        L1 = generaVectorAleatorio(n1)
-        L2 = generaVectorAleatorio(n2)
+    p1 = generaVectorAleatorio(n)
+    p2 = generaVectorAleatorio(n)
+    p3 = generaVectorAleatorio(n)
 
-        L3 = sumaDosVectores(L1, L2)
+    prom = []
+    for i in range(n):
+        prom.append( (p1[i] + p2[i] + p3[i])/3 )
 
-        muestraVectores(L1, L2, L3)
+    print(f'Alumno\t', end='')
+    for i in range(3):
+        print(f'P{i+1}\t', end='')
+    print('Promedio\n')
 
-
-    else:
-        print("Error! No pueden sumarse...")
+    for i in range(n):
+        print(f'{i+1}\t{p1[i]}\t{p2[i]}\t{p3[i]}\t{prom[i]}\t')
