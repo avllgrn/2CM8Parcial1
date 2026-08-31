e96af1d004bccdd629239462cec31371f18f6c9a
avllgrn <avillagran@ipn.mx>
2026-08-31 12:28:24 -0600

    Suma de vectores con funciones

--- ejemplo.py
+++ ejemplo.py
@@ -1,6 +1,24 @@
 from subprocess import run
 from random import randrange
 
+def generaVectorAleatorio(n):
+    V = []
+    for i in range(n):
+        V.append( randrange(100) )
+    return V
+
+def sumaDosVectores(A, B):
+    n = len(A)
+    C = []
+    for i in range(n):
+        C.append( A[i] + B[i] )    
+    return C
+
+def muestraVectores(X, Y, Z):
+    n = len(X)
+    for i in range(n):
+        print(f'[{i}]\t{X[i]} + {Y[i]}\t= {Z[i]}')
+
 if __name__=='__main__':
     run('cls', shell=True)
 
@@ -8,28 +26,13 @@ if __name__=='__main__':
     n2 = int(input('Dame n2 '))
 
     if n1==n2:
-        L1 = []
-        for i in range(n1):
-            L1.append( randrange(100) )
-        print()
+        L1 = generaVectorAleatorio(n1)
+        L2 = generaVectorAleatorio(n2)
 
-        L2 = []
-        for i in range(n2):
-            L2.append( randrange(100) )
-        print()
+        L3 = sumaDosVectores(L1, L2)
 
-        n3 = n1
-        L3 = []
-        for i in range(n3):
-            L3.append( L1[i] + L2[i] )
+        muestraVectores(L1, L2, L3)
 
-        print(f'L1={L1}')
-        print(f'L2={L2}')
-        print(f'L3={L3}')
-        print()
-
-        for i in range(n3):
-            print(f'[{i}]\t{L1[i]} + {L2[i]}\t= {L3[i]}')
 
     else:
         print("Error! No pueden sumarse...")
