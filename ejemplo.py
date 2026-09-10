c8d04d10d7cbdaa493761c7ff1ebb5721452b81c
avllgrn <avillagran@ipn.mx>
2026-09-10 15:54:31 -0600

    Genera y muestra matriz (m filas, n columnas) con ceros

--- ejemplo.py
+++ ejemplo.py
@@ -2,50 +2,23 @@ from subprocess import run
 
 if __name__=='__main__':
     run('cls', shell=True)
-    M = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
 
-    print(M, type(M))
+    m = int(input('Filas? '))
+    n = int(input('Columnas? '))
+
+    # Se genera una matriz de m filas por n columnas
+    M = []
+    for i in range(m):
+        fila = []
+        for j in range(n):
+            fila.append( 0 ) # Llena de ceros
+        M.append( fila )
+
+    print(M)
     print()
 
-    print(M[0], type(M[0]))
-    print(M[1], type(M[1]))
-    print(M[2], type(M[2]))
-    print()
-
-    print(M[0][0], type(M[0][0]))
-    print(M[0][1], type(M[0][1]))
-    print(M[0][2], type(M[0][2]))
-    print()
-
-    print(M[1][0], type(M[1][0]))
-    print(M[1][1], type(M[1][1]))
-    print(M[1][2], type(M[1][2]))
-    print()
-
-    print(M[2][0], type(M[2][0]))
-    print(M[2][1], type(M[2][1]))
-    print(M[2][2], type(M[2][2]))
-    print()
-
-
-    print(M[0][0], end='\t')
-    print(M[0][1], end='\t')
-    print(M[0][2], end='\t')
-    print()
-
-    print(M[1][0], end='\t')
-    print(M[1][1], end='\t')
-    print(M[1][2], end='\t')
-    print()
-
-    print(M[2][0], end='\t')
-    print(M[2][1], end='\t')
-    print(M[2][2], end='\t')
-    print()
-
-    print('\n\n')
-
-    for i in range(3):
-        for j in range(3):
+    # Se muestra una matriz de 
+    for i in range(m): # m filas por 
+        for j in range(n): # n columnas
             print(M[i][j], end='\t')
         print()
