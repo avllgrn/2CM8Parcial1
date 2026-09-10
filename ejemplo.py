1118a9b73c955081a656b92bef621ccd8acbb42c
avllgrn <avillagran@ipn.mx>
2026-09-10 12:16:14 -0600

    Matriz (lista de listas) 3x3

--- ejemplo.py
+++ ejemplo.py
@@ -1,14 +1,44 @@
 from subprocess import run
-from random import randrange
 
 if __name__=='__main__':
     run('cls', shell=True)
+    M = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]
 
-    m = 5
-    n = 10
+    print(M, type(M))
+    print()
 
-    for i in range( m ):
-        for j in range( n ):
-            print(f'[{i}][{j}]', end='\t')
+    print(M[0], type(M[0]))
+    print(M[1], type(M[1]))
+    print(M[2], type(M[2]))
+    print()
 
-        print('\n')
+    print(M[0][0], type(M[0][0]))
+    print(M[0][1], type(M[0][1]))
+    print(M[0][2], type(M[0][2]))
+    print()
+
+    print(M[1][0], type(M[1][0]))
+    print(M[1][1], type(M[1][1]))
+    print(M[1][2], type(M[1][2]))
+    print()
+
+    print(M[2][0], type(M[2][0]))
+    print(M[2][1], type(M[2][1]))
+    print(M[2][2], type(M[2][2]))
+    print()
+
+    print(M[0][0], end='\t')
+    print(M[0][1], end='\t')
+    print(M[0][2], end='\t')
+    print()
+
+    print(M[1][0], end='\t')
+    print(M[1][1], end='\t')
+    print(M[1][2], end='\t')
+    print()
+
+    print(M[2][0], end='\t')
+    print(M[2][1], end='\t')
+    print(M[2][2], end='\t')
+    print()
+
