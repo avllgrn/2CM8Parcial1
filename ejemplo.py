c1ccfd26e4071c603e17ab3927fbe66556b7caaf
avllgrn <avillagran@ipn.mx>
2026-09-18 06:40:19 -0600

    Genera Matriz

--- ejemplo.py
+++ ejemplo.py
@@ -1,136 +1,84 @@
 from subprocess import run
 from random import randrange
 
-def generaMatriz(m,n):
-    # Se genera una matriz de m filas por n columnas
-    M = []
-    for i in range(m):
-        fila = []
-        for j in range(n):
-            fila.append( 0 ) # Llena de ceros
-        M.append( fila )
-    return M
-
 def muestraMatriz(X):
     m = len(X)
     n = len(X[0])
-    # Se muestra una matriz de 
-    for i in range(m): # m filas por 
-        for j in range(n): # n columnas
+    for i in range(m):
+        for j in range(n):
             print(X[i][j], end='\t')
         print()
 
-def rellenaConContador(X):
-    m = len(X)
-    n = len(X[0])
-    cont = 1
-    for i in range(m):
-        for j in range(n):
-            X[i][j] = cont
-            cont += 1
-
-def rellenaIdentidad(X):
-    n = len(X)
-    for i in range(n):
-        for j in range(n):
-            if i==j:
-                X[i][j] = 1
-            else:
-                X[i][j] = 0
-
-def generaMatrizAleatorios(m,n):
-    # Se genera una matriz de m filas por n columnas
+def generaMatrizCeros(m, n):
     M = []
     for i in range(m):
         fila = []
         for j in range(n):
-            fila.append( randrange(100) ) # Llena de ceros
-        M.append( fila )
+            fila.append( 0 )
+        M.append(fila)
     return M
 
-def copiaMatriz(X):
-    m = len(X)
-    n = len(X[0])
-
-    Copia = []
+def generaMatrizAleatorios(m, n):
+    M = []
     for i in range(m):
         fila = []
         for j in range(n):
-            fila.append(X[i][j])
-        Copia.append( fila )
-    return Copia
+            fila.append( randrange(100) )
+        M.append(fila)
+    return M
 
-def rellenaTranspuesta(MT, M):
-    m = len(M)
-    n = len(M[0])
-
+def generaMatrizConteo(m, n):
+    M = []
+    cont = 1
     for i in range(m):
+        
+        fila = []
         for j in range(n):
-            MT[j][i] = M[i][j]
+            fila.append( cont )
+            cont+=1
+        M.append(fila)
+    return M
+
+def generaMatrizContFila(m, n):
+    M = []
+    for i in range(m):
+        fila = []
+        for j in range(n):
+            fila.append( i )
+        M.append(fila)
+    return M
+
+def generaMatrizContColumna(m, n):
+    M = []
+    for i in range(m):
+        fila = []
+        for j in range(n):
+            fila.append( j )
+        M.append(fila)
+    return M
 
 if __name__=='__main__':
     run('cls', shell=True)
 
-    print('Genera Matriz con ceros y después cambiarlos por un contador\n\n')
-    m = int(input('Filas? '))
-    n = int(input('Columnas? '))
+    filas = int(input('Cuántas filas? '))
+    columnas = int(input('Cuántas columas? '))
 
-    # Genera matriz
-    M = generaMatriz(m,n)
-    muestraMatriz(M)
+    M1 = generaMatrizCeros(filas, columnas)
+    muestraMatriz(M1)
     print()
 
-    # Rellenala con un contador
-    rellenaConContador(M)
-    muestraMatriz(M)
-    print()
-    input('Presiona Enter para continuar...')
-    run('cls', shell=True)
-
-
-    print('Genera Matriz con ceros y después cambiarla por Identidad\n\n')
-    n = int(input('Filas y Columnas? '))
-    # Genera matriz
-    I = generaMatriz(n,n)
-    muestraMatriz(I)
+    M2 = generaMatrizAleatorios(filas, columnas)
+    muestraMatriz(M2)
     print()
 
-    rellenaIdentidad(I)
-    muestraMatriz(I)
-    print()
-    input('Presiona Enter para continuar...')
-    run('cls', shell=True)
-
-
-    print('Copiar una Matriz\n\n')
-    m = int(input('Filas? '))
-    n = int(input('Columnas? '))
-
-    # Genera matriz
-    Original = generaMatrizAleatorios(m,n)
-    muestraMatriz(Original)
+    M3 = generaMatrizConteo(filas, columnas)
+    muestraMatriz(M3)
     print()
 
-    C = copiaMatriz(Original)
-    muestraMatriz(C)
-    print()
-    input('Presiona Enter para continuar...')
-    run('cls', shell=True)
-
-
-    print('Genera Matriz Transpuesta\n\n')
-    m = int(input('Filas? '))
-    n = int(input('Columnas? '))
-
-    # Genera matriz
-    Matriz = generaMatrizAleatorios(m,n)
-    muestraMatriz(Matriz)
+    M4 = generaMatrizContFila(filas, columnas)
+    muestraMatriz(M4)
     print()
 
-    mT = n
-    nT = m
-    Transpuesta = generaMatriz(mT, nT)
-    rellenaTranspuesta(Transpuesta, Matriz)
-    muestraMatriz(Transpuesta)
+    M5 = generaMatrizContColumna(filas, columnas)
+    muestraMatriz(M5)
     print()
-
